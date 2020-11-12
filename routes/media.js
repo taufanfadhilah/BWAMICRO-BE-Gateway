a84f2a222f520a683f6f2ec335e50ec47879965b
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2020-11-12 21:29:49 +0700

    integrate Media Service

--- routes/media.js
+++ routes/media.js
@@ -1,9 +1,10 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
-/* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("media");
-});
+const mediaHandler = require("./handler/media");
+
+router.post("/", mediaHandler.create);
+router.get("/", mediaHandler.getAll);
+router.delete("/:id", mediaHandler.destroy);
 
 module.exports = router;
